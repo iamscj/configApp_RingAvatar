8fce4c9f15e8325d921551e3aaabb383b1bcdeb0
Shreyasa Joshi <shreyasa.joshi@ringcentral.com>
2025-05-09 17:06:19 +0530

    config app fix

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const express = require("express");
-const cors = require("cors");
+import express from "express";
+import cors from "cors";
+
 const app = express();
 
 // In-memory variable to act as a key-value store
